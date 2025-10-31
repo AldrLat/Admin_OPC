b9eb4f80876af8ff9a60be6cca7a0f2152413eb5
AleksandrWork <aldrlatrus@mail.ru>
2025-10-31 14:59:24 +0500

    Функцию проверки, что запущен какой-либо процесс, перенесли из UnitConnectControllers в UnitDM

--- UnitDM.pas
+++ UnitDM.pas
@@ -6,7 +6,7 @@ uses
   System.SysUtils, System.Classes, Vcl.Controls, Vcl.Forms, Winapi.Windows, Data.DB,
   Data.Win.ADODB, System.Variants, Vcl.ComCtrls, Registry, WinSvc, OPCDA, OPCHDA, Vcl.StdCtrls,
   Vcl.Graphics, Winapi.Messages, Printers, VCLTee.Chart, Vcl.Dialogs, Winapi.ShellAPI,
-  RudaGlobals;
+  tlhelp32, RudaGlobals;
 
 const
       DefaultColorEdit = $00DEC4B0; //clCream; //цвет объектов в режиме редактирования
@@ -213,7 +213,7 @@ type
     function FunTypeController: integer;
     function SelectionByControllerType(ControllerID: Int64; TypeController: integer): boolean;   //выбор по типу контроллера
     function SendDataSet(CDS: TCopyDataStruct): integer;  //передать собщение всем окнам
-
+    function IsRunning(sName: string): boolean;
   private
     { Private declarations }
 
@@ -1400,6 +1400,35 @@ begin
     end;
     Printer.EndDoc;
   end;
+end;
+
+function TDM.IsRunning(sName: string): boolean; // проверяет, запущен ли процесс sName
+var
+  han: THandle;
+  ProcStruct: PROCESSENTRY32; // from "tlhelp32" in uses clause
+  sID: string;
+begin
+  Result := false;
+  // Get a snapshot of the system
+  han := CreateToolhelp32Snapshot(TH32CS_SNAPALL, 0);
+  if han = 0 then exit;
+  // Loop thru the processes until we find it or hit the end
+  ProcStruct.dwSize := sizeof(PROCESSENTRY32);
+  if Process32First(han, ProcStruct) then
+  begin
+    repeat
+      sID := ExtractFileName(ProcStruct.szExeFile);
+      // Check only against the portion of the name supplied, ignoring case
+      if uppercase(copy(sId, 1, length(sName))) = uppercase(sName) then
+      begin
+        // Report we found it
+        Result := true;
+        Break;
+      end;
+    until not Process32Next(han, ProcStruct);
+  end;
+  // clean-up
+  CloseHandle(han);
 end;
 
 end.
